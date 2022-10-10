a6c93622764f019c1ed3948a8481f95988abfcf1
palminski <williambolls98@gmail.com>
2022-10-10 12:04:18 -0700

    added all API requests

---
+++ controllers/api/comment-routes.js
@@ -0,0 +1,27 @@
+const router = require('express').Router();
+const { User, Post, Comment } = require('../../models'); //grabs from index file
+//<><><><><><><><><><><><>
+
+router.post('/', (req,res) => {
+
+    // if (req.session) {
+        console.log(req.body)
+        Comment.create(
+            {
+                text_content: req.body.text_content,
+                user_id: req.body.user_id, //Replace with session ID
+                post_id: req.body.post_id
+            }
+        )
+        .then(commentData => {
+            res.json(commentData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        });
+    // }
+});
+
+//<><><><><><><><><><><><>
+module.exports = router;
--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -3,7 +3,46 @@ const { User, Post, Comment } = require('../../models'); //grabs from index file
 //<><><><><><><><><><><><><><><><>
 
 //GET--------------------------
+//Get all posts
+router.get('/', (req,res) => {
+    Post.findAll({
 
+    })
+    .then(postData => res.json(postData))
+    .catch( err => {
+        console.log(err);
+        res.status(500).json(err);
+    })
+});
+
+//Get user by ID
+router.get('/:id', (req,res) => {
+    Post.findOne({
+        include: [
+            {
+                model: Comment,
+                attributes:['id', 'text_content', 'post_id', 'user_id', 'created_at'],
+                include:{
+                    model: User,
+                    attributes: ['username']
+                }
+            }
+        ],
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(postData => {
+        if (!postData) {
+            res.status(404).json({ message: "Uh Oh, Post not found"});
+        }
+        res.json(postData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err)
+    })
+})
 //POST-------------------------
 //create a new post
 router.post('/',(req,res) => {
